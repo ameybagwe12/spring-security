d2d17cdea0915a9c1fb400aa9f3bb46c3a221260
Amey Bagwe <ameybagwe09@gmail.com>
2024-10-01 13:09:20 +0530

    Setup for security

---
+++ src/main/java/com/amey/spring_security/HelloController.java
@@ -0,0 +1,12 @@
+package com.amey.spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public String greet(){
+        return "Hello World";
+    }
+}
