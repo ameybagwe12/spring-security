bcbb6826a22ab7c8ae9ab1c6b98e9e68cb4106b7
Amey Bagwe <ameybagwe09@gmail.com>
2024-10-04 18:59:04 +0530

    JWT code added

--- src/main/java/com/amey/spring_security/config/SecurityConfig.java
+++ src/main/java/com/amey/spring_security/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -60,11 +62,16 @@ public class SecurityConfig {
         // DISABLING METHOD
         // MAKING SESSION STATELESS
         http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request.requestMatchers("register", "login").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 
 //    @Bean
--- src/main/java/com/amey/spring_security/controller/UserController.java
+++ src/main/java/com/amey/spring_security/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.amey.spring_security.controller;
 
 import com.amey.spring_security.model.User;
+import com.amey.spring_security.service.JwtService;
 import com.amey.spring_security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
+    private JwtService jwtService;
+
+    @Autowired
     private UserService service;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @PostMapping("register")
     public User register(@RequestBody User user){
         return service.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken(user.getUsername());
+        else
+            return "Login Failed";
     }
 
 }
